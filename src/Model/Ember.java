4bfb6d332a5d65a7df18831642bad86edfc9a93a
kompleeex <paaladam1@gmail.com>
2023-02-27 13:47:38 +0100

    fajlbeolvasas

--- src/Model/Ember.java
+++ src/Model/Ember.java
@@ -1,5 +1,13 @@
 package Model;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ember {
     private String nev;
     private int kor;
@@ -7,11 +15,36 @@ public class Ember {
     private int MunkToltEV;
 
     public Ember(String nev, int kor, String neme, int MunkToltEV) {
-        this.nev = nev;
-        this.kor = kor;
-        this.neme = neme;
-        this.MunkToltEV = MunkToltEV;
+        this(nev+";"+kor+";"+neme+";"+MunkToltEV);
     }
+    
+    public Ember(String sor){
+        this(sor, ";");
+    }
+    
+    public Ember(String sor, String sep){
+        String[] s = sor.split(sep);
+        this.nev =s[0];
+        this.kor =Integer.parseInt(s[1]);
+        this.neme = s[2];
+        this.MunkToltEV =Integer.parseInt(s[3]);
+    }
+    
+    public void fajlbeolvasas() throws IOException{
+        String fn = "Dolgozat.txt";
+        Path path = Paths.get(fn);
+        List<String> sorok = Files.readAllLines(path);
+        
+        String fejlec = sorok.get(0);
+        sorok.remove(0);
+        
+        List<Ember> emberek = new ArrayList<>();
+        for (String sor : sorok) {
+            emberek.add(new Ember(sor, ":"));
+        }
+        
+    }
+    
 
     public String getNev() {
         return nev;
@@ -44,6 +77,8 @@ public class Ember {
     public void setMunkToltEV(int MunkToltEV) {
         this.MunkToltEV = MunkToltEV;
     }
+    
+
     
     
 }
